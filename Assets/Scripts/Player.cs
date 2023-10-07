2431d1684400675a204aceff8f39066ffbdcd8d8
ButzkeGWG <josef.s@gowithguide.com>
2023-10-07 21:14:04 +0900

    main music and sound effects

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,8 @@ public class Player : MonoBehaviour, IKitchenObjectParent
         get; private set;
     }
 
+    public event EventHandler OnPickedSomething;
+
     public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
     public class OnSelectedCounterChangedEventArgs : EventArgs
     {
@@ -169,6 +171,11 @@ public class Player : MonoBehaviour, IKitchenObjectParent
     public void SetKitchenObject(KitchenObject kitchenObject)
     {
         this.kitchenObject = kitchenObject;
+
+        if(kitchenObject != null)
+        {
+            OnPickedSomething?.Invoke(this, EventArgs.Empty);
+        }
     }
 
     public KitchenObject GetKitchenObject()
